e8fb181f62e113de1e6bb6178a7ed641c99c8b31
Jochen Hilgers <jochen.hilgers@gmail.com>
2011-12-09 19:37:33 +0100

    working on layout

--- src/restoo/MainBundle/Controller/DefaultController.php
+++ src/restoo/MainBundle/Controller/DefaultController.php
@@ -6,14 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * 
+ * @author jochen
+ */
 class DefaultController extends Controller
 {
 	/**
-	 * @Route("/")
+	 * @Route("/",name="home")
 	 * @Template()
 	 */
 	public function indexAction()
 	{
 		return array();
 	}
+	
+	/**
+	 * 
+	 * @Route( "/help", name="help" )
+	 * @Template()
+	 */
+	public function helpAction()
+	{
+		return array();
+	}
+	
 }
